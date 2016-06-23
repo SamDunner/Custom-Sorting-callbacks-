6cf2ebee0703d8cc3156dc20061a9bbb4c54ba2a
sam dunner <samdunner23@gmail.com>
2016-06-23 19:53:03 +0000

    outline of custom sorting assignment

---
+++ custom-sorting.js
@@ -0,0 +1,16 @@
+/* exercise 1:
+Write a function that outputs this: [1, 2, 5, 9, 10]
+
+
+
+
+/* exercise 2:
+
+var students = [
+  { id: 1, name: "bruce",    age: 20 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 30 },
+  { id: 4, name: "alex",     age: 22 }
+];
+// the ids of sorted array should be ordered [3, 4, 1, 2]
+
