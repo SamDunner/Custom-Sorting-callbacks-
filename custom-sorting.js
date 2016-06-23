8948583b74693a70f45b059ffa6c6bbf4363b584
sam dunner <samdunner23@gmail.com>
2016-06-23 20:36:55 +0000

    changed the ages in the previous example to better test, reverted to original assignment here

--- custom-sorting.js
+++ custom-sorting.js
@@ -25,8 +25,8 @@ var students = [
 var students = [
   { id: 1, name: "bruce",    age: 20 },
   { id: 2, name: "zoidberg", age: 22 },
-  { id: 3, name: "alex",     age: 22 },
-  { id: 4, name: "alex",     age: 30 }
+  { id: 3, name: "alex",     age: 30 },
+  { id: 4, name: "alex",     age: 22 }
 ];
 var result = students.sort(function(a, b){
   var nameA = a.name;
