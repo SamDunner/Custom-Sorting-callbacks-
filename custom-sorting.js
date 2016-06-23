58b8c6016844ba5700e3e1345d34a33ff6b7dd71
sam dunner <samdunner23@gmail.com>
2016-06-23 20:30:58 +0000

    sort function in place, need to add to accomodate age

--- custom-sorting.js
+++ custom-sorting.js
@@ -20,3 +20,24 @@ var students = [
 ];
 // the ids of sorted array should be ordered [3, 4, 1, 2]
 
+*/
+
+var students = [
+  { id: 1, name: "bruce",    age: 20 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 30 },
+  { id: 4, name: "alex",     age: 22 }
+];
+var result = students.sort(function(a, b){
+  var nameA = a.name;
+  var nameB = b.name;
+  if (nameA < nameB) {
+    return -1;
+  } else if (nameA > nameB) {
+    return 1;
+  } else {
+    return 0;
+  }
+});
+console.log(result);
+
